44b4106737d252549fe1769db94aa5e2dc365c6b
Pranil Bhosale <63090882+bhosalepranil16@users.noreply.github.com>
2022-01-13 13:00:06 +0530

    Time: 64 ms (7.30%), Space: 17.6 MB (32.73%) - LeetHub

---
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        int noOfChildren = g.size(), noOfCookies = s.size();
+        int i = 0, j = 0;
+        
+        int res = 0;
+        
+        while(i < noOfChildren && j < noOfCookies) {
+            if(s[j] >= g[i]) {
+                res++;
+                i++;
+                j++;
+            } else {
+                j++;    
+            }
+        }
+        
+        return res;
+    }
+};

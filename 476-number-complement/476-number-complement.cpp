f6c8877e5d11e021f526e29556bca920b06b947a
Pranil Bhosale <63090882+bhosalepranil16@users.noreply.github.com>
2022-01-17 04:18:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.27%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int res = 0, i = 0;
+        while(n > 0) {
+            int rem = n % 2;
+            if(rem == 0) {
+                res += pow(2, i);
+            }
+            i++;
+            n = n / 2;
+        }
+        return res;
+    }
+};

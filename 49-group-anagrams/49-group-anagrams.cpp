5a21fdaa23fad04098e5fb9da32f28fa5a6b6e47
Pranil Bhosale <63090882+bhosalepranil16@users.noreply.github.com>
2022-01-30 12:52:35 +0530

    Time: 80 ms (20.70%), Space: 20.6 MB (46.58%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        vector<pair<string, int> > arr;
         vector<vector<string> > res;
         unordered_map<string, vector<string> > temp;
         int n = strs.size();
         
         for(int i=0;i<n;i++) {
-            arr.push_back(make_pair(strs[i], i));
-            sort(arr[i].first.begin(), arr[i].first.end());
-        }
-        
-        for(int i=0;i<n;i++) {
-            // cout << arr[i].first << " " << strs[arr[i].second] <<endl;
-            temp[arr[i].first].push_back(strs[arr[i].second]);
+            string x = strs[i];
+            sort(x.begin(), x.end());
+            temp[x].push_back(strs[i]);
         }
         
         for(pair<string, vector<string> > a : temp) {
